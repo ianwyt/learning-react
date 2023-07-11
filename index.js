9e38bd3093ebd36530798d14b9b9fcfa20b35665
ianwyt <mihailsmirnov2511@gmail.com>
2023-07-11 19:01:37 +0300

    Updated component

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ import ReactDOM from "react-dom"
 function Page() {
     return (
         <div>
-            <img src="./react-logo.png" width = "50" height = "50"/>
+            <header>
+                <nav>
+                    <img src="./react-logo.png" width = "50" height = "50"/>
+                </nav>
+            </header>
             <h1>My awesome website in React</h1>
             <h3>Reasons I love React</h3>
             <ol>
@@ -13,6 +17,9 @@ function Page() {
                 <li>It's a hireable skill</li>
                 <li>It's actively maintained by skilled people</li>
             </ol>
+            <footer>    
+                <small>© 2023 Ianwyt. All rights reserved.</small>
+            </footer>
         </div>
     )
 }
