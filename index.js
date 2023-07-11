fa601bb19cd25ac6ece39f51e6aaec752b257276
ianwyt <mihailsmirnov2511@gmail.com>
2023-07-11 18:23:41 +0300

    First website

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
+/*
+Challenge: Starting from scratch, build and render the 
+HTML for our section project. Check the Google slide for 
+what you're trying to build.
 
-const navbar = (
-    <nav>
-        <h1>Bob's Bistro</h1>
-        <ul>
-            <li>Menu</li>
-            <li>About</li>
-            <li>Contact</li>
-        </ul>
-    </nav>
+We'll be adding styling to it later.
+
+Hints:
+* The React logo is a file in the project tree, so you can
+  access it by using `src="./react-logo.png" in your image
+  element
+* You can also set the `width` attribute of the image element
+  just like in HTML. In the slide, I have it set to 40px
+ */
+
+import React from "react"
+import ReactDOM from "react-dom"
+
+const page = (
+    <div>
+        <img src="./react-logo.png" width = "50" height = "50"/>
+        <h1>My awesome website in React</h1>
+        <h3>Reasons I love React</h3>
+        <ol>
+            <li>It's composable</li>
+            <li>It's declarative</li>
+            <li>It's a hireable skill</li>
+            <li>It's actively maintained by skilled people</li>
+        </ol>
+    </div>
 )
 
-// ReactDOM.render(navbar, document.getElementById("root"))
-ReactDOM.createRoot(document.getElementById("root")).render(navbar)
+ReactDOM.render(page, document.getElementById("root"))
