ac6bcb3ce0c2aa70c5bc46999204016ad3cb3356
ianwyt <mihailsmirnov2511@gmail.com>
2023-07-14 00:30:51 +0300

    Learning React - Project 1 (Done)

--- learning-react-project/src/components/Header.jsx
+++ learning-react-project/src/components/Header.jsx
@@ -1,18 +1,16 @@
-import React from "react"
+import React from "react";
+import logo from "../assets/sticker.webp";
 
 function Header() {
     return (
         <header>
             <nav className="nav">
-                <img src="./react-logo.png" className="nav-logo" />
-                <ul className="nav-items">
-                    <li>Pricing</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
+                <img src={logo} className="nav--logo" alt="React Logo" />
+                <h3 className="nav--logo_text">AboutMe</h3>
+                <h4 className="nav--title">Learning React - Project 1</h4>
             </nav>
         </header>
-    )
+    );
 }
 
 export default Header
---
+++ learning-react-project/src/components/Main.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+function Main() {
+    return (
+        <div className="container">
+            <h1 className="main--title">Five facts about me</h1>
+            <ul className="main--facts">
+                <li>I was born in 2001</li>
+                <li>My hair is long and curly</li>
+                <li>I hold a brown belt in karate</li>
+                <li>I write poetry and music</li>
+                <li>Some people think that my last name is named after vodka</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Main
